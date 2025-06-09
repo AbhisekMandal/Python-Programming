da5990fbc86896d4675dcac474434e2ad08d9abe
Abhisek <mandalabhisek20@gmail.com>
2025-06-09 23:29:59 +0530

    Reverse String using python

---
+++ ReverseString.py
@@ -0,0 +1,9 @@
+#Approch-1
+line = input("Enter a string.")
+reversed_line = line[::-1]
+print(reversed_line)
+#Approch-2
+line2 = "Python"
+reversed_line = "".join(reversed(line2))
+print(reversed_line)
+#Approch-3
